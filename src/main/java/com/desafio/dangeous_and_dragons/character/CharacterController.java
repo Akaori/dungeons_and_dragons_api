3faa0203c727899907918a628f271d92ac977e1c
akaori <akiyamaanaluiza@gmail.com>
2023-09-09 20:53:06 -0300

    add controller

--- src/main/java/com/desafio/dangeous_and_dragons/character/CharacterController.java
+++ src/main/java/com/desafio/dangeous_and_dragons/character/CharacterController.java
@@ -1,15 +1,39 @@
 package com.desafio.dangeous_and_dragons.character;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.desafio.dangeous_and_dragons.character.dto.CharacterRequestDTO;
+import com.desafio.dangeous_and_dragons.character.dto.CharacterResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/v1/characters", produces = "application/json")
+@RequiredArgsConstructor
 public class CharacterController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    private final CharacterService characterService;
+
+    @GetMapping
+    public ResponseEntity<List<CharacterResponseDTO>> findAll() {
+        return ResponseEntity.ok().body(characterService.getAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CharacterResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(characterService.findById(id));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id) {
+        characterService.delete(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<CharacterResponseDTO> save(@RequestBody CharacterRequestDTO character) {
+        var createdCharacter = characterService.save(character);
+        return ResponseEntity.ok().body(createdCharacter);
     }
 
 }
