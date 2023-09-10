4fff9e86b61afe1dca690f6d7b01ae2b7fcbd87a
akaori <akiyamaanaluiza@gmail.com>
2023-09-09 23:41:19 -0300

    add instructions to connect pgadmin to postgres

--- src/main/java/com/desafio/dungeonsanddragons/DungeonsAndDragonsApplication.java
+++ src/main/java/com/desafio/dungeonsanddragons/DungeonsAndDragonsApplication.java
@@ -1,4 +1,4 @@
-package com.desafio.dangeous_and_dragons;
+package com.desafio.dungeonsanddragons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
