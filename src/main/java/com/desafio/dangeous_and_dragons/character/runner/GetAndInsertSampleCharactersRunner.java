ddb99320acdace84280ab33e56dd74ae661c86d1
akaori <akiyamaanaluiza@gmail.com>
2023-09-09 22:32:24 -0300

    add script to add sample data at the app startup

---
+++ src/main/java/com/desafio/dangeous_and_dragons/character/runner/GetAndInsertSampleCharactersRunner.java
@@ -0,0 +1,33 @@
+package com.desafio.dangeous_and_dragons.character.runner;
+
+import com.desafio.dangeous_and_dragons.character.CharacterModel;
+import com.desafio.dangeous_and_dragons.character.CharacterRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class GetAndInsertSampleCharactersRunner  implements CommandLineRunner {
+
+    private final CharacterRepository characterRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(characterRepository.count() > 1) return;
+
+        // read json and write to db
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<CharacterModel>> typeReference = new TypeReference<List<CharacterModel>>(){};
+        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/sample_characters.json");
+
+        List<CharacterModel> characters = mapper.readValue(inputStream,typeReference);
+        characterRepository.saveAll(characters);
+        System.out.println("Sample characters Saved!");
+    }
+}
