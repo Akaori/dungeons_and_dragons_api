c709ae15355d34d269b3af3edfbbc1521aacce33
akaori <akiyamaanaluiza@gmail.com>
2023-09-11 20:44:53 -0300

    move configs to the same folder

--- src/main/java/com/desafio/dungeonsanddragons/config/SecurityConfig.java
+++ src/main/java/com/desafio/dungeonsanddragons/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.desafio.dungeonsanddragons.auth;
+package com.desafio.dungeonsanddragons.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
