ccb010b3143ba558ec0ab38c140860828bcaef30
akaori <akiyamaanaluiza@gmail.com>
2023-09-13 06:53:03 -0300

    add query to select monsters ids

--- src/main/java/com/desafio/dungeonsanddragons/character/CharacterRepository.java
+++ src/main/java/com/desafio/dungeonsanddragons/character/CharacterRepository.java
@@ -1,6 +1,11 @@
 package com.desafio.dungeonsanddragons.character;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CharacterRepository extends JpaRepository<CharacterModel, Long> {
+    @Query(value = "select c.id from CharacterModel c where c.role = 1")
+    List<Long> findMonstersIds();
 }
